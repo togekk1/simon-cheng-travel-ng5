a2d2060cf52bf7cc149dd7dfa2e47c48150bdc31
togekk <togekk@gmail.com>
2017-12-03 17:14:27 +0800

    Add intro animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { trigger, style, transition, animate, state } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less', '../assets/fonts/stylesheet.css'],
+  animations: [
+    trigger('pre', [
+      state("on", style({ opacity: 1, transform: 'translateY(0)' })),
+      transition(":enter", animate('1s 4s cubic-bezier(0, .5, .5, 1)')),
+      transition(":leave", animate('.7s ease-in', style({ opacity: 0, transform: 'translateY(-10px)' })
+      )
+    ]),
+    trigger('name', [
+      state("on", style({ opacity: 1, transform: 'translateY(0)' })),
+      transition(":enter", animate('1s 5s cubic-bezier(0, .5, .5, 1)'))
+      transition(":leave", animate('.5s ease-in', style({ opacity: 0, transform: 'translateY(-10px)' })
+    ]),
+    trigger('enter', [
+      state("on", style({ opacity: 1, transform: 'translateX(0)' })),
+      transition(":enter", animate('300ms 7s cubic-bezier(0, .5, .5, 1)'))
+      transition(":leave", animate('.3s ease-in', style({ opacity: 0, transform: 'translateX(-5px)' })
+    ])
+  ]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  disabled: Boolean;
+  leave: Boolean;
+
+  constructor() {
+    this.disabled = true;
+  }
+  ngOnInit() {
+    // let animation = { opacity: [0, 1], transform: ['translateY(20px)', 'translateY(0)'] };
+    // let ease = { easing: 'cubic-bezier(0, .5, .5, 1)', fill: 'forwards' };
+    // document.querySelector('.pre').animate(animation, Object.assign({ duration: 1000, delay: 4000 }, ease));
+    // document.querySelector('.name').animate(animation, Object.assign({ duration: 1000, delay: 5000 }, ease));
+    // const enter_animation = document.querySelector('.enter').animate({ opacity: [0, 1], transform: ['translateX(5px)', 'translateX(0)'] },
+    //   Object.assign({ duration: 300, delay: 7000 }, ease));
+    // enter_animation.onfinish = () => {
+    //   this.disabled = false;
+    // };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
@@ -13,6 +13,7 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : []
   ],
   providers: [],
