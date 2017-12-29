db9c37bfeccdb949fc087ec3d6d2b8d69f0e56ba
togekk <togekk@gmail.com>
2017-12-29 10:23:08 +0800

    Add travel content page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,71 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { trigger, style, transition, animate, state } from '@angular/animations';
-import { Options } from 'selenium-webdriver';
-import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
-import { Observable } from 'rxjs/Observable';
-import * as firebase from 'firebase/app';
+import { Component, Input, OnInit, HostListener, Inject } from "@angular/core";
+import {
+  trigger,
+  style,
+  transition,
+  animate,
+  state
+} from "@angular/animations";
+import { Options } from "selenium-webdriver";
+import { AngularFireDatabase, AngularFireList } from "angularfire2/database";
+import { Observable } from "rxjs/Observable";
+import * as firebase from "firebase/app";
+import { DOCUMENT } from "@angular/platform-browser";
+import { WINDOW } from "./window.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less', '../assets/fonts/stylesheet.css'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.less", "../assets/fonts/stylesheet.css"],
   animations: [
-    trigger('pre', [
-      state('on', style({ opacity: 1, transform: 'translateY(0)' })),
-      transition(':enter', animate('1s 4s cubic-bezier(0, .5, .5, 1)')),
-      transition(':leave', animate('.7s ease-in', style({ opacity: 0, transform: 'translateY(-10px)' })))
+    trigger("bg_intro", [
+      state("on", style({ opacity: 1, transform: "translateX(-10px)" })),
+      transition(":enter", animate("2s 1s ease-out")),
+      transition(
+        ":leave",
+        animate(
+          ".8s ease-in",
+          style({ opacity: 0, transform: "translateX(-20px)" })
+        )
+      )
     ]),
-    trigger('name', [
-      state('on', style({ opacity: 1, transform: 'translateY(0)' })),
-      transition(':enter', animate('1s 5s cubic-bezier(0, .5, .5, 1)')),
-      transition(':leave', animate('.5s ease-in', style({ opacity: 0, transform: 'translateY(-10px)' })))
+    trigger("pre", [
+      state("on", style({ opacity: 1, transform: "translateY(0)" })),
+      transition(":enter", animate("1s 4s cubic-bezier(0, .5, .5, 1)")),
+      transition(
+        ":leave",
+        animate(
+          ".7s ease-in",
+          style({ opacity: 0, transform: "translateY(-10px)" })
+        )
+      )
     ]),
-    trigger('enter', [
-      state('on', style({ opacity: 1, transform: 'translateX(0)' })),
-      transition(':enter', animate('300ms 7s cubic-bezier(0, .5, .5, 1)')),
-      transition(':leave', animate('.3s ease-in', style({ opacity: 0, transform: 'translateX(-5px)' })))
+    trigger("name", [
+      state("on", style({ opacity: 1, transform: "translateY(0)" })),
+      transition(":enter", animate("1s 5s cubic-bezier(0, .5, .5, 1)")),
+      transition(
+        ":leave",
+        animate(
+          ".5s ease-in",
+          style({ opacity: 0, transform: "translateY(-10px)" })
+        )
+      )
+    ]),
+    trigger("enter", [
+      state("on", style({ opacity: 1, transform: "translateX(0)" })),
+      transition(":enter", animate("300ms 7s cubic-bezier(0, .5, .5, 1)")),
+      transition(
+        ":leave",
+        animate(
+          ".3s ease-in",
+          style({ opacity: 0, transform: "translateX(-5px)" })
+        )
+      )
+    ]),
+    trigger("text", [
+      state("on", style({ opacity: 1 })),
+      transition(":enter", animate("1s linear")),
+      transition(":leave", animate("2s linear", style({ opacity: 0 })))
     ])
   ]
 })
@@ -32,22 +74,57 @@ export class AppComponent implements OnInit {
   leave: Boolean;
   background: any;
   intro_bg: string;
+  intro_hide: Boolean;
+  data: Array<any> = [];
+  scrolling_offset: number;
 
-  constructor(public afDb: AngularFireDatabase) {
+  constructor(
+    public afDb: AngularFireDatabase,
+    @Inject(DOCUMENT) private document: Document,
+    @Inject(WINDOW) private window
+  ) {
     this.disabled = true;
-    afDb.list('background').valueChanges().subscribe(res => {
-      this.background = res;
-      this.render_bg();
-    });
+    this.intro_hide = false;
+    this.document.documentElement.scrollTop = 0;
+    // const self = this;
+    afDb
+      .list("background")
+      .valueChanges()
+      .subscribe(res => {
+        // console.log(res);
+        this.background = res;
+        this.render_bg();
+      });
+    afDb
+      .list("data/en/data")
+      .valueChanges()
+      .subscribe(res => {
+        res.forEach(data => {
+          this.data.push(data);
+        });
+        // this.render_page();
+      });
+  }
+
+  render_bg() {
+    this.intro_bg =
+      "url(" + this.background[this.background.length - 1].org + ")";
+  }
+
+  handleDone(event: AnimationTransitionEvent) {
+    console.log(event.toState);
+    if (event.toState === "void") this.intro_hide = true;
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    this.scrolling_offset = this.window.pageYOffset;
   }
 
   ngOnInit() {
-
     // const completedPercentage = [];
     // let sum;
-
     // for (let i = 0; i < items.length; i++) {
-
     //   const xmlHTTP = new XMLHttpRequest();
     //   xmlHTTP.open('GET', items[i], true);
     //   xmlHTTP.responseType = 'arraybuffer';
@@ -60,27 +137,19 @@ export class AppComponent implements OnInit {
     //     sum = parseInt(completedPercentage.reduce((a, b) => a + b, 0) /
     //       items.length);
     //     this.loading_percentage = sum;
-
     //     if (sum === 100) {
     //       // this.intro_hidden = false;
     //       // const loading_animation = this.$$('.loading').animate({ opacity: [1, 0] }, { duration: 1000, fill: 'forwards', delay: 2000 })
     //       // loading_animation.onfinish = () => {
     //       //   this.loading_hidden = true;
     //       // };
-
     //       console.log('Loading complete')
     //     };
-
     //   };
     //   xmlHTTP.onloadstart = () => {
     //     completedPercentage[i] = 0;
     //   };
     //   xmlHTTP.send();
-
     // }
-  }
-
-  render_bg() {
-    this.intro_bg = "url(" + this.background[this.background.length - 1].org + ")";
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { AppComponent } from "./app.component";
 
-import { environment } from '../environments/environment';
+import { environment } from "../environments/environment";
 
-import { AngularFireModule } from 'angularfire2';
+import { AngularFireModule } from "angularfire2";
 
 // New imports to update based on AngularFire2 version 4
-import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireDatabaseModule } from "angularfire2/database";
+
+import { WINDOW_PROVIDERS } from "./window.service";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDLC2pZ8gUAxVBJInHk0MFVjQzUsuzTI-g",
-  authDomain: "simon-cheng-1f786.firebaseapp.com",
-  databaseURL: "https://simon-cheng-1f786.firebaseio.com",
-  projectId: "simon-cheng-1f786",
-  storageBucket: "simon-cheng-1f786.appspot.com",
-  messagingSenderId: "792982216746"
+  apiKey: "AIzaSyB6tIkSAFsEb8cZQSYq4tlOpuw9DWaF-_E",
+  authDomain: "simon-cheng-64272.firebaseapp.com",
+  databaseURL: "https://simon-cheng-64272.firebaseio.com",
+  projectId: "simon-cheng-64272",
+  storageBucket: "simon-cheng-64272.appspot.com",
+  messagingSenderId: "330596747932"
 };
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : [],
+    environment.production
+      ? ServiceWorkerModule.register("/ngsw-worker.js")
+      : [],
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireDatabaseModule
   ],
-  providers: [],
+  providers: [WINDOW_PROVIDERS],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
