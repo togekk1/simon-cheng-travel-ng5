10cdb80b67d86aa95961d72c7dcf8aa220b81428
togekk <togekk@gmail.com>
2018-11-17 13:31:32 +0800

    Remove unnecessary service worker registration

--- src/main.ts
+++ src/main.ts
@@ -8,17 +8,6 @@ if (environment.production) {
   enableProdMode();
 }
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/ngsw-worker.js")
-    .then(function(registration) {
-      console.log("Service Worker registered");
-    })
-    .catch(function(err) {
-      console.log("Service Worker registration failed: ", err);
-    });
-}
-
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch(err => console.log(err));
